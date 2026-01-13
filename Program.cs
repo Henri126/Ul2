5998812c74479ce0e39fb247265a7cbb89ccbcf8
Opilane TTHK <opilane@tthk.ee>
2026-01-13 09:58:48 +0200

    0..00

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string tuba = "";
+Console.WriteLine("Mis on teie ruumi kuju? Kas kolmnurk, Ristkülik või Ring");
+tuba = Console.ReadLine();
+
+if (tuba == "kolmnurk")
+{
+    int arv1 = 0;
+    int arv2 = 0;
+    int arv3 = 0;
+    Console.WriteLine("Mis on teie ruumi esimene külg");
+
+}
